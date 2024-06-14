f8de0ab7b19872c0a274e525da8cf6cda37afbd7
elenakalistova <lenka2001kalistova@gmail.com>
2024-06-15 02:25:35 +0300

    add update employee

--- OutOfOffice/Controllers/EmployeeController.cs
+++ OutOfOffice/Controllers/EmployeeController.cs
@@ -30,11 +30,12 @@ namespace OutOfOffice.Controllers
         public IActionResult AddEmployee()
         {
             ViewBag.PeoplePartners = new SelectList(_dbContext.Employees.Where(e => e.Position == Position.HR), "Id", "FullName");
+
             return View();
         }
 
         [HttpPost]
-        public IActionResult PostEmployee(Employee employee)
+        public IActionResult PostAddEmployee(Employee employee)
         {
             if (ModelState.IsValid)
             {
@@ -44,8 +45,62 @@ namespace OutOfOffice.Controllers
             }
 
             ViewBag.PeoplePartners = new SelectList(_dbContext.Employees.Where(e => e.Position == Position.HR), "Id", "FullName");
+
             return View("AddEmployee", employee);
         }
+
+        [HttpGet]
+        public IActionResult UpdateEmployee(int employeeID)
+        {
+            var employee = _dbContext.Employees.Find(employeeID);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.PeoplePartners = new SelectList(_dbContext.Employees.Where(e => e.Position == Position.HR), "Id", "FullName", employee.PeoplePartnerId);
+
+            return View("UpdateEmployee", employee);
+        }
+
+        [HttpPost]
+        public IActionResult PostUpdateEmployee(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var existingEmployee = _dbContext.Employees.Find(employee.Id);
+                    if (existingEmployee == null)
+                    {
+                        return NotFound();
+                    }
+
+                    existingEmployee.FullName = employee.FullName;
+                    existingEmployee.Subdivision = employee.Subdivision;
+                    existingEmployee.Position = employee.Position;
+                    existingEmployee.EmployeeStatus = employee.EmployeeStatus;
+                    existingEmployee.PeoplePartnerId = employee.PeoplePartnerId;
+                    existingEmployee.Balance = employee.Balance;
+
+                    _dbContext.Update(existingEmployee);
+                    _dbContext.SaveChanges();
+
+                    return RedirectToAction("ViewEmployees");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Error updating employee: " + ex.Message);
+                }
+            }
+
+            ViewBag.PeoplePartners = new SelectList(_dbContext.Employees.Where(e => e.Position == Position.HR), "Id", "FullName", employee.PeoplePartnerId);
+
+            return View("UpdateEmployee", employee);
+        }
+
     }
+
 }
+
 
